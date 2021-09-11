238659ca49ea9a55e87e2bab64ad88d9708d579a
Sean Friedowitz <sfriedowitz@citrine.io>
2021-09-10 20:51:40 -0700

    trying to auto diff

--- src/main/scala/AutoDiff.scala
+++ src/main/scala/AutoDiff.scala
@@ -2,101 +2,101 @@ import spire.math._
 import spire.implicits._
 import spire.algebra._
 
-import breeze.linalg.{DenseVector, DenseMatrix}
-
 import scala.reflect.ClassTag
 
-abstract class AutoDiffFunction(val inputSize: Int, val outputSize: Int) {
+protected abstract class AutoDiffFunction(val inputSize: Int,
+                                          val outputSize: Int) {
   require(inputSize > 0, s"Input size must be positive: $inputSize")
   require(outputSize > 0, s"Output size must be positive: $outputSize")
 
   implicit val jetDim: JetDim = JetDim(inputSize)
-
 }
 
-abstract class VectorAutoDiffFunction(inputSize: Int, outputSize: Int)
-    extends AutoDiffFunction(inputSize, outputSize) {
-
-  def apply[T: Field: Trig: NRoot: ClassTag](
-      x: DenseVector[T]
-  ): DenseVector[T]
-
-  def jacobian(x: DenseVector[Double]): DenseMatrix[Double] = {
-    val J = DenseMatrix.zeros[Double](inputSize, outputSize)
-    AutoDiff.evaluateJacobian(this, x, J)(this.jetDim)
-    J
-  }
-
-  def jacobian(x: DenseVector[Double], J: DenseMatrix[Double]): Unit = {
-    AutoDiff.evaluateJacobian(this, x, J)(this.jetDim)
-  }
-
-}
-
-abstract class ScalarAutoDiffFunction(inputSize: Int)
+abstract class AutoDiffScalarFunction(inputSize: Int)
     extends AutoDiffFunction(inputSize, 1) { self =>
 
-//  lazy private val vectorFunction =
-//    new VectorAutoDiffFunction(inputSize, inputSize) {
+//  lazy private val vectorFunction: AutoDiffVector =
+//    new AutoDiffVector(inputSize, inputSize) {
 //      override def apply[T: Field: Trig: NRoot: ClassTag](
-//          x: DenseVector[T]
-//      ): DenseVector[T] = {
-//        self.gradient(x)
+//        x: Array[T]
+//      ): Array[T] = {
+//        val g = Array.ofDim[T](x.length)
+//        AutoDiff.evaluateGradient(self.apply[Jet[T]], x, g)
+//        g
 //      }
 //    }
 
-  def apply[T: Field: Trig: NRoot: ClassTag](
-      x: DenseVector[T]
-  ): T
+  def apply[T: Field: Trig: NRoot: ClassTag](x: Array[T]): T
 
-  def gradient(x: DenseVector[Double]): DenseVector[Double] = {
-    val g = DenseVector.zeros[Double](inputSize)
-    gradient(x, g)
-    g
+  def gradient(x: Array[Double]): Array[Double] = {
+    AutoDiff.evaluateGradient[Double](this.apply, x)
   }
 
-  def gradient(x: DenseVector[Double], g: DenseVector[Double]): Unit = {
-    AutoDiff.evaluateGradient(this, x, g)(this.jetDim)
+  def gradient(x: Array[Double], g: Array[Double]): Unit = {
+    AutoDiff.evaluateGradient[Double](this.apply, x, g)
   }
 
 }
 
-private object AutoDiff {
+abstract class AutoDiffVectorFunction(inputSize: Int, outputSize: Int)
+    extends AutoDiffFunction(inputSize, outputSize) {
 
-  def evaluateGradient(
-      func: ScalarAutoDiffFunction,
-      x: DenseVector[Double],
-      g: DenseVector[Double]
-  )(implicit dim: JetDim): Unit = {
-    val m = func.inputSize
+  def apply[T: Field: Trig: NRoot: ClassTag](x: Array[T]): Array[T]
 
-    val jets = DenseVector.zeros[Jet[Double]](m)
-    cforRange(0 until m) { i =>
-      jets(i) = x(i) + Jet.h[Double](i)
+  def jacobian(x: Array[Double]): Array[Array[Double]] = {
+    val J = Array.ofDim[Double](inputSize, outputSize)
+    AutoDiff.evaluateJacobian[Double](this.apply, x, J)
+    J
+  }
+
+  def jacobian(x: Array[Double], J: Array[Array[Double]]): Unit = {
+    AutoDiff.evaluateJacobian[Double](this.apply, x, J)
+  }
+
+}
+
+object AutoDiff {
+
+  def toJets[T: Field: Trig: NRoot: ClassTag](
+    x: Array[T]
+  )(implicit dim: JetDim): Array[Jet[T]] = {
+    val jets = Array.ofDim[Jet[T]](x.length)
+    cforRange(x.indices) { i =>
+      jets(i) = Jet[T](x(i), i)
     }
+    jets
+  }
 
+  def evaluateGradient[T: Field: Trig: NRoot: ClassTag](
+    func: Array[Jet[T]] => Jet[T],
+    x: Array[T],
+    g: Array[T]
+  )(implicit dim: JetDim): Unit = {
+    val jets = toJets(x)
     val res = func(jets)
-    cforRange(0 until m) { i =>
+    cforRange(x.indices) { i =>
       g(i) = res.infinitesimal(i)
     }
   }
 
-  def evaluateJacobian(
-      func: VectorAutoDiffFunction,
-      x: DenseVector[Double],
-      J: DenseMatrix[Double]
+  def evaluateGradient[T: Field: Trig: NRoot: ClassTag](
+    func: Array[Jet[T]] => Jet[T],
+    x: Array[T]
+  )(implicit dim: JetDim): Array[T] = {
+    val g = Array.ofDim[T](x.length)
+    evaluateGradient(func, x, g)
+    g
+  }
+
+  def evaluateJacobian[T: Field: Trig: NRoot: ClassTag](
+    func: Array[Jet[T]] => Array[Jet[T]],
+    x: Array[T],
+    J: Array[Array[T]]
   )(implicit dim: JetDim): Unit = {
-    val m = func.inputSize
-    val n = func.outputSize
-
-    val jets = DenseVector.zeros[Jet[Double]](m)
-    cforRange(0 until m) { j =>
-      jets(j) = Jet[Double](x(j), j)
-    }
-
+    val jets = toJets(x)
     val res = func(jets)
-    cforRange2(0 until m, 0 until n) { (i, j) =>
-      J(i, j) = res(i).infinitesimal(j)
+    cforRange2(x.indices, res.indices) { (i, j) =>
+      J(i)(j) = res(j).infinitesimal(i)
     }
   }
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,34 @@
-import breeze.linalg.DenseVector
-
+import breeze.linalg.{DenseMatrix, DenseVector}
 import spire.algebra._
 import spire.implicits._
+import spire.math.{Jet, JetDim}
 
 import scala.reflect.ClassTag
 
 object Main extends App {
 
-  case class SimpleFunction(k: Double) extends ScalarAutoDiffFunction(2) {
-    override def apply[T: Field: Trig: NRoot: ClassTag](
-        x: DenseVector[T]
-    ): T = {
-      x(0) * x(0) * x(1) + k * Trig[T].cos(x(0) * x(1))
+  case class ScalarFunction(k: Double) extends AutoDiffScalarFunction(2) {
+    override def apply[T: Field: Trig: NRoot: ClassTag](x: Array[T]): T = {
+      x(0) * x(0) * x(1) + 4 * spire.math.exp(-spire.math.cos(x(1)))
     }
   }
 
-  val x = DenseVector(4.0, 2.0)
-  val f = SimpleFunction(10.0)
+  case class VectorFunction() extends AutoDiffVectorFunction(2, 2) {
+    override def apply[T: Field: Trig: NRoot: ClassTag](
+      x: Array[T]
+    ): Array[T] = {
+      Array(x(0) + x(0) * x(1), x(1) * x(1))
+    }
+  }
 
-  println(f.gradient(x))
+  implicit val d: JetDim = JetDim(1)
+  val x1 = Jet[Double](0.0, 0)
+  val x2 = Jet[Double](4.0, 0)
+
+  val x = Array(0.0, 4.0)
+  val g = Array(0.0, 0.0)
+  val f = ScalarFunction(5.0)
+  f.gradient(x, g)
+  println(DenseVector(g: _*))
 
 }
